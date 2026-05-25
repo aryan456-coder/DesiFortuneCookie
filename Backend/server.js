8e7a9a869ce8df9d3198d423ba48f9a1bb1839b1
[aryanpatade09@gmail.com] <[aryanpatade09@gmail.com]>
2026-05-25 18:45:43 +0530

    Add port fallback

--- Backend/server.js
+++ Backend/server.js
@@ -151,6 +151,8 @@ app.delete("/api/reviews/:id", (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 5000; 
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
