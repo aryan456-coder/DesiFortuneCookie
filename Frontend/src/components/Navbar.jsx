4c7c03c7bab4a86b9b59e25869189d5f9394492f
[aryanpatade09@gmail.com] <[aryanpatade09@gmail.com]>
2026-08-23 21:44:13 +0530

    making mobile friendly

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar()
     return  <nav>
             <div className="nav-logo">🍪Aunty ji knows the best</div>
             <ul className="nav-links">
-                <li><a href="#how">how it works</a></li>
+                <li><a href="#how">How it works</a></li>
                 <li><a href="#crack">Crack a cookie</a></li>
                 <li><a href="#sample">Sample fortunes</a></li>
             </ul>
